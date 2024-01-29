a80ce477b318e8d048cc0d75a7ef95a47cd4c633
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-01-29 10:48:50 -0500

    routing practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component:HomeComponent},
+  {path:'aboutUs', component: AboutUsComponent },
+  {path:'contact', component: ContactComponent},
+  {path: 'view/:id', component: ViewComponent},
+  {path:'**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
