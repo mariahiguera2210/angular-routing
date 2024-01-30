86d10835b233d521c2c1b2bbb97d77f31bccc9eb
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-01-29 19:04:21 -0500

    lazy load was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path:'home', component:HomeComponent},
-  {path:'aboutUs', component: AboutUsComponent },
-  {path:'contact', component: ContactComponent},
   {path: 'view/:id', component: ViewComponent},
+  { path: 'AboutUs', loadChildren: () => import('./about-us/about-us.module').then(m => m.AboutUsModule) },
+  { path: 'Contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
   {path:'**', component: NotFoundComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ViewComponent } from './view/view.component';
 
@@ -14,9 +12,7 @@ import { ViewComponent } from './view/view.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutUsComponent,
     HomeComponent,
-    ContactComponent,
     NotFoundComponent,
     ViewComponent
   ],
